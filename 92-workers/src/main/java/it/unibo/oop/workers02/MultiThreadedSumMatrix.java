1570e1256d0197a988486b7343254ea1a0527256
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2022-11-23 16:34:41 +0100

    Created the class MultiThreadedSumMatrix

---
+++ 92-workers/src/main/java/it/unibo/oop/workers02/MultiThreadedSumMatrix.java
@@ -0,0 +1,11 @@
+package it.unibo.oop.workers02;
+
+public class MultiThreadedSumMatrix implements SumMatrix {
+
+    @Override
+    public double sum(double[][] matrix) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+}
