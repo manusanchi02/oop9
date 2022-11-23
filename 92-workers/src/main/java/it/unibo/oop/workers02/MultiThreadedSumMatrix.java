70d8f4e702aa14caa0b7d892bf59bc12f4c5606e
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2022-11-23 16:36:49 +0100

    Create the constructor

--- 92-workers/src/main/java/it/unibo/oop/workers02/MultiThreadedSumMatrix.java
+++ 92-workers/src/main/java/it/unibo/oop/workers02/MultiThreadedSumMatrix.java
@@ -2,6 +2,11 @@ package it.unibo.oop.workers02;
 
 public class MultiThreadedSumMatrix implements SumMatrix {
 
+    int threads;
+    public MultiThreadedSumMatrix(int threads) {
+        this.threads = threads;
+    }
+
     @Override
     public double sum(double[][] matrix) {
         // TODO Auto-generated method stub
