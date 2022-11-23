50f506940126216794de88332903e5675388b147
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2022-11-23 16:56:08 +0100

    Some attempt

--- 92-workers/src/main/java/it/unibo/oop/workers02/MultiThreadedSumMatrix.java
+++ 92-workers/src/main/java/it/unibo/oop/workers02/MultiThreadedSumMatrix.java
@@ -12,8 +12,34 @@ public class MultiThreadedSumMatrix implements SumMatrix {
 
     @Override
     public double sum(double[][] matrix) {
-        // TODO Auto-generated method stub
-        return 0;
+        Worker worker = new Worker(0, 10, matrix);
+        return worker.res;
+    }
+
+    private static class Worker extends Thread {
+        private final int startpos;
+        private final int nelem;
+        private long res;
+        private double[][] m;
+
+        /**
+        * Builds a new worker
+        */
+        Worker(int startpos, int nelem, double[][] m) {
+            super();
+            this.startpos = startpos;
+            this.nelem = nelem;
+            this.m = m;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Working from position " + startpos + " to position " + (startpos + nelem - 1));
+            for (int i = startpos; i < m.length && i < startpos + nelem; i++) {
+                this.res += Double.doubleToLongBits(m[i][i]);
+            }
+        }
+
     }
     
 }
